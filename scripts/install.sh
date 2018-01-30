1834f65cb7d87d3bbb8fe1d7ac9b77778dfcb381
jakoblorz <jakob@lorz.org>
2018-01-30 11:27:23 +0100

    initialized scripts

---
+++ scripts/install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# MIT License
+
+# Copyright (c) 2018 Jakob Lorz
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+export GOPATH="$(pwd)"
+export GOBIN="$(pwd)/bin"
+
+
+if ! [ -f "packages" ] ; then
+    touch "packages"
+fi
+
+if [ $# -eq 0 ] ; then
+    cat "packages" | while read in; do
+        if [ -n "$in" ] ; then
+            go get $in
+            echo "go get $in"
+        fi
+    done
+
+    
+    cat "packages" >> "packages.temp"
+    cat "packages.temp" | sed '/^$/d' > "packages"
+    rm "packages.temp"
+    
+    exit 0
+fi
+
+go get $*
+
+echo "\n$*" >> "packages"
+
+cat "packages" >> "packages.temp"
+cat "packages.temp" | sed '/^$/d' > "packages"
+rm "packages.temp"
