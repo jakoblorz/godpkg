b2eb49f6fe9af7317b47da65be0ae5ff7261c276
jakoblorz <jakob@lorz.org>
2018-01-30 12:22:32 +0100

    successfully created first cli

--- src/godpkg.go
+++ src/godpkg.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/mitchellh/cli"
 )
 
-func main() {
-	c := cli.NewCli("godpkg", "1.0.0")
-	c.Args := os.Args[1:]
+type InstallCommand struct {
+}
 
+func (*InstallCommand) Help() string {
+	return "install dependency"
+}
+
+func (*InstallCommand) Run(args []string) int {
+	fmt.Printf("install, %v\n", args)
+	return 0
+}
+
+func (command *InstallCommand) Synopsis() string {
+	return command.Help()
+}
+
+func main() {
+	c := cli.NewCLI("godpkg", "1.0.0")
+	c.Args = os.Args[1:]
 
 	install := func() (cli.Command, error) {
-
+		return &InstallCommand{}, nil
 	}
 
 	c.Commands = map[string]cli.CommandFactory{
-		"install": install
+		"install": install,
 	}
+
+	status, err := c.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(status)
 }
